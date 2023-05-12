a93895ca9e701f482390fd6cfae3ea5f68fef628
Mohit Sharma <101691278+itsmohit124@users.noreply.github.com>
2023-05-12 17:33:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+const form = document.querySelector("#task-form");
+        const input = document.querySelector("#task-input");
+        const list_el = document.querySelector("#tasks");
+
+        let todos = JSON.parse(localStorage.getItem('todos')) || [];
+
+        // function to render tasks
+        function renderTasks() {
+            list_el.innerHTML = '';
+            todos.forEach((task) => {
+                const task_el = document.createElement("div");
+                task_el.classList.add("task");
+
+                const task_content_el= document.createElement("div");
+                task_content_el.classList.add("content");
+
+                task_el.appendChild(task_content_el);
+
+                const task_input_el= document.createElement("input");
+                task_input_el.classList.add("text");
+                task_input_el.type = "text";
+                task_input_el.value = task.content;
+                task_input_el.setAttribute("readonly", "readonly");
+
+                task_content_el.appendChild(task_input_el);
+
+                const task_actions_el = document.createElement("div");
+                task_actions_el.classList.add("actions");
+
+                const task_edit_el = document.createElement("button");
+                task_edit_el.classList.add("edit");
+                task_edit_el.innerHTML="Edit";
+
+                const task_delete_el = document.createElement("button");
+                task_delete_el.classList.add("delete");
+                task_delete_el.innerHTML="Delete";
+
+                task_actions_el.appendChild(task_edit_el);
+                task_actions_el.appendChild(task_delete_el);
+
+                task_el.appendChild(task_actions_el);
+
+                list_el.appendChild(task_el);
+
+                task_edit_el.addEventListener('click', () => {
+                    if (task_edit_el.innerText.toLowerCase() == "edit") {
+                        task_input_el.removeAttribute("readonly");
+                        task_input_el.focus();
+                        task_edit_el.innerText = "Save";
+                    } else {
+                        task_input_el.setAttribute("readonly", "readonly");
+                        task_edit_el.innerText = "Edit";
+                    }
+                    task.content = task_input_el.value;
+                    localStorage.setItem('todos', JSON.stringify(todos));
+                });
+
+                task_delete_el.addEventListener('click', () => {
+                    todos = todos.filter((t) => t !== task);
+                    localStorage.setItem('todos', JSON.stringify(todos));
+                    list_el.removeChild(task_el);
+                });
+            });
+        }
+
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const task = {
+                content: input.value,
+                done: false,
+                createdAt: new Date().getTime()
+            };
+
+            todos.push(task);
+
+            localStorage.setItem('todos', JSON.stringify(todos));
+
+            input.value = "";
+
+            renderTasks();
+        });
+
+        renderTasks();
